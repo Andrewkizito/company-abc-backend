b705072296f39d51036175f77d08117cc76fec47
Andrewkizito <Andrewkizito54@gmail.com>
2023-02-26 16:10:51 +0300

    Models - Order

---
+++ models/Order.ts
@@ -0,0 +1,51 @@
+import { model, Schema } from 'mongoose';
+
+const OrderItemSchema = new Schema({
+	productName: {
+		type: String,
+		required: true
+	},
+	quantity: {
+		type: Number,
+		required: true,
+		min: 1
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	unit: {
+		type: String,
+		required: true,
+	},
+});
+
+const OrderSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	phoneNumber: {
+		type: String,
+		required: true,
+	},
+	location: {
+		type: String,
+		required: true,
+	},
+	cart: {
+		type: [OrderItemSchema],
+		required: true
+	},
+	orderStatus: {
+		type: String,
+		enum: ['APPROVED','REJECTED','COMPLETED','PENDING'],
+		default: 'PENDING'
+	},
+	subscriptionArn: {
+		type: String,
+		required: true,
+	},
+});
+
+export default model('orders', OrderSchema);
