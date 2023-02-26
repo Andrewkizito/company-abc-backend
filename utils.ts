59f618f129de5d582cc8b4221d5ef00f96ad336a
Andrewkizito <Andrewkizito54@gmail.com>
2023-02-26 16:09:18 +0300

    Added Utility Functions

---
+++ utils.ts
@@ -0,0 +1,16 @@
+export interface IFile {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  buffer: Buffer;
+  size: number;
+}
+
+export type callbackFunction = (error: unknown,data?: unknown) => unknown;
+
+export type fileHandlerFunction = (
+  req: unknown,
+  file: IFile,
+  cb: callbackFunction
+) => void;
