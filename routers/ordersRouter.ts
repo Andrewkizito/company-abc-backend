7b9b53aa1940b6146876c99265a467e6045392fa
Andrewkizito <Andrewkizito54@gmail.com>
2023-02-26 20:26:34 +0300

    Routers - Added approve order path

--- routers/ordersRouter.ts
+++ routers/ordersRouter.ts
@@ -1,11 +1,35 @@
 import { Request, Response, Router } from 'express';
+import { approveOrder, getOrders, placeOrder } from '../controllers/orders';
+import { checkAuthState } from '../controllers/auth';
 import { createSubscription } from '../controllers/messaging';
-import { placeOrder } from '../controllers/orders';
 
 const ordersRouter = Router();
 
-ordersRouter.post('/', createSubscription, placeOrder, (req: Request, res: Response) => {
-	res.send(req.body).status(200);
-});
+ordersRouter.post(
+	'/',
+	createSubscription,
+	placeOrder,
+	(req: Request, res: Response) => {
+		res.send(req.body).status(200);
+	}
+);
+
+ordersRouter.get(
+	'/',
+	checkAuthState,
+	getOrders,
+	(req: Request, res: Response) => {
+		res.send(req.body).status(200);
+	}
+);
+
+ordersRouter.patch(
+	'/approve',
+	checkAuthState,
+	approveOrder,
+	(req: Request, res: Response) => {
+		res.send(req.body).status(200);
+	}
+);
 
 export default ordersRouter;
