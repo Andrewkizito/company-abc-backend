15c4b917a9f9414622acecdcda191a951d483135
Andrewkizito <Andrewkizito54@gmail.com>
2023-02-26 16:11:04 +0300

    Models - Product

---
+++ models/Product.ts
@@ -0,0 +1,34 @@
+import { model, Schema } from "mongoose";
+
+const ProductSchema = new Schema({
+  product_name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  unit: {
+    type: String,
+    required: true,
+  },
+});
+
+export default model("products", ProductSchema);
