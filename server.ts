fa504a44b19f46dfb50282f3c6953388842a292f
Andrewkizito <Andrewkizito54@gmail.com>
2023-02-27 11:32:59 +0300

    Added delete method to cors

--- server.ts
+++ server.ts
@@ -24,7 +24,7 @@ const app = express();
 //Setting up cors
 app.use(function (_, res: Response, next: NextFunction) {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
