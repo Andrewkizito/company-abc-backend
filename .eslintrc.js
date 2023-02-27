80f059bd38ccef1c3d45ca4617799f77e9768555
Andrewkizito <Andrewkizito54@gmail.com>
2023-02-27 15:01:20 +0300

    Added ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,20 @@ module.exports = {
 	env: {
 		es2021: true,
 		node: true,
-		jest: true
+		jest: true,
 	},
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
 	overrides: [],
-	parser: '@typescript-eslint/parser',
+	parser: "@typescript-eslint/parser",
 	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
+		ecmaVersion: "latest",
+		sourceType: "module",
 	},
-	plugins: ['@typescript-eslint'],
+	plugins: ["@typescript-eslint"],
 	rules: {
-		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
 	},
 };
